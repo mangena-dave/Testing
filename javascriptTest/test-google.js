4b73510c73084bf776fef107c049c6a0c98e1a17
shaeen <shaeen@rapidtrade.biz>
2017-08-03 16:31:26 +0200

    created a test script that goes to google and searchs a text

---
+++ javascriptTest/test-google.js
@@ -0,0 +1,31 @@
+// mocha test-google.js --timeout 600000
+var chai, expect, selenium, waitFor;
+
+selenium = require("selenium-webdriver");
+chai = require('chai');
+chai.use(require('chai-as-promised'));
+expect = chai.expect;
+
+before(function() {
+  global.driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
+  global.waittime = 6000;
+  global.url = 'https://google.co.za';
+  driver.getWindowHandle();
+  driver.get(url);
+  return driver.sleep(1000);
+});
+
+describe('Google test', function() {
+  it('search for text', function() {
+    driver.findElement({id: 'lst-ib'}).sendKeys("Nissan GTR");
+    driver.sleep(500);
+    driver.findElement({xpath: "//div[@id='sbse0']/div[@class='sbqs_c']"}).click();
+    return driver.sleep(100);
+  });
+});
+
+waitFor = function(obj, message) {
+  return driver.wait((function() {
+    return driver.isElementPresent(obj);
+  }), waittime, "\n" + message);
+};
