427e78c563647843e0f3942685f282a2ed24c70c
playdave <you@example.com>
2017-08-14 13:51:54 +0200

    modify .json

--- test/test-google.js
+++ test/test-google.js
@@ -23,6 +23,10 @@ describe('Google test', function() {
     // driver.findElement({xpath: "//div[@id='sbse0']/div[@class='sbqs_c']"}).click();
     return driver.sleep(500);
   });
+
+ it('Quit the browser', function(){
+	driver.quit();
+   });
 });
 
 waitFor = function(obj, message) {
