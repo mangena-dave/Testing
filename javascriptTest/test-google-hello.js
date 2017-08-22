e0b6f624045aff89825f3cad98cad492cb675706
mangena-dave <david@rapidtrade.biz>
2017-08-22 15:54:53 +0200

    Update test-google-hello.js

--- javascriptTest/test-google-hello.js
+++ javascriptTest/test-google-hello.js
@@ -21,7 +21,6 @@ describe('Google test', function() {
     driver.findElement({id: 'lst-ib'}).sendKeys("hello");
 //     driver.sleep(500);
 //     driver.findElement({xpath:"//div[@id='sbse0']/div[@class='sbqs_c']"}).click();
-	  //
     return driver.sleep(500);
   });
 	
