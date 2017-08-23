6ecf8f4d4ec7b930071d37c7440afc2fd086485b
playdave <you@example.com>
2017-08-23 11:01:14 +0200

    testing

--- javascriptTest/test-google-hello.js
+++ javascriptTest/test-google-hello.js
@@ -21,10 +21,8 @@ describe('Google test', function() {
     driver.findElement({id: 'lst-ib'}).sendKeys("hello");
 //     driver.sleep(500);
 //     driver.findElement({xpath:"//div[@id='sbse0']/div[@class='sbqs_c']"}).click();
-	  //
     return driver.sleep(500);
   });
-	
 
   it('Quit Browser', function() {
     return driver.quit();
