72e06a468cbe6341f17e19a6a7cabf06fee40728
Dave <david@rapidtrade.biz>
2019-02-13 15:49:12 +0200

    testing

--- javascriptTest/test-google-hello.js
+++ javascriptTest/test-google-hello.js
@@ -18,7 +18,7 @@ before(function() {
 describe('Google test', function() {
   it('search for text', function() {
     driver.findElement({id: 'lst-ib'}).clear();
-    driver.findElement({id: 'lst-ib'}).sendKeys("hello");
+    driver.findElement({id: 'lst-ib'}).sendKeys("testing ");
   //driver.sleep(500);
   //driver.findElement({xpath:"//div[@id='sbse0']/div[@class='sbqs_c']"}).click();
     return driver.sleep(500);
